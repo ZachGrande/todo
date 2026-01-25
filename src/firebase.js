a52fccb766af302cbee037b7e2556dc03ca64d0f
Zach Grande <zach.grande23@gmail.com>
2026-01-25 14:09:30 -0800

    chore: set up firebase config, firestore rules

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCSv2B3-pSl_Ncvz-F7r9mAugdwRw31Fec",
+  authDomain: "todo-1f5ed.firebaseapp.com",
+  projectId: "todo-1f5ed",
+  storageBucket: "todo-1f5ed.firebasestorage.app",
+  messagingSenderId: "1410968366",
+  appId: "1:1410968366:web:fdfff95f6d7c96ce8b68bf",
+  measurementId: "G-YMV54PSSV2"
+};
+
+const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app)
+export const db = getFirestore(app)
+export const googleProvider = new GoogleAuthProvider()
